4619f101cd60c420f74bbe935bad05b26960571b
Subira Wafubwa <subira.craig.2023@gmail.com>
2024-06-24 21:31:21 +0300

    Last Commit

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code in this file!
+let currentUser = 'Subira';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+let userInitial = currentUser.slice(0,1);
+let shortMessage = welcomeMessage.slice(0,7);
+let shortGreeting  = `${shortMessage}, ${userInitial}!`;
